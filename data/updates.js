a6110895ae9c33dc1f5bc5ffae5e311496196f47
Velizar Ivanov <zivanof@gmail.com>
2019-05-31 22:49:34 +0300

    Added mocked user and updates data

---
+++ data/updates.js
@@ -0,0 +1,28 @@
+const updatesData = [
+  {
+    id: 1,
+    content: 'update 1',
+    tags: [],
+    user: {
+      id: 1
+    }
+  },
+  {
+    id: 2,
+    content: 'update 2',
+    tags: [],
+    user: {
+      id: 1
+    }
+  },
+  {
+    id: 3,
+    content: 'update 3',
+    tags: [],
+    user: {
+      id: 2
+    }
+  }
+];
+
+export default updatesData;
