b935a3e7905ae179139aebac6f0a5efb8a955f64
SunFrogThe <turtaleta@gmail.com>
2019-09-04 12:02:02 +0300

    :recycle: refactored hello-reducer.js

--- src/reducers/hello-reducer.js
+++ src/reducers/hello-reducer.js
@@ -7,22 +7,25 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case ACTION_ONE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         message: action.payload.message
-      });
+      };
     case CONT2_LOADING:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         message: 'cont2 loading...'
-      });
+      };
     case CONT2_LOADED:
       console.log('CONT2 LOADED REDUCER');
       console.log(JSON.stringify(action.payload, null, 2));
 
-      return Object.assign({}, state, {
+      return {
+        ...state,
         userId: action.payload.userId,
         title: action.payload.title,
         message: 'cont2 loaded!'
-      });
+      };
     default:
       return state;
   }
